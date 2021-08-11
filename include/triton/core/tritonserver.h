0d081da58b1be0a699bc1af80ec91253d461c424
Tanmay Verma <tanmay2592@gmail.com>
2021-08-11 13:53:45 -0700

    Bump tritonserver minor version for adding rate limiter APIs (#28)

--- include/triton/core/tritonserver.h
+++ include/triton/core/tritonserver.h
@@ -87,7 +87,7 @@ struct TRITONSERVER_ServerOptions;
 ///   }
 ///
 #define TRITONSERVER_API_VERSION_MAJOR 1
-#define TRITONSERVER_API_VERSION_MINOR 3
+#define TRITONSERVER_API_VERSION_MINOR 4
 
 /// Get the TRITONBACKEND API version supported by the Triton shared
 /// library. This value can be compared against the
