647ea17602a828a73931eb072b56e2aa704dcd74
Ashwin Ramesh <asramesh@nvidia.com>
2021-09-08 12:08:18 -0700

    Added TRITONSERVER_ServerOptionsSetMetricsInterval

--- include/triton/core/tritonserver.h
+++ include/triton/core/tritonserver.h
@@ -1424,6 +1424,16 @@ TRITONSERVER_DECLSPEC TRITONSERVER_Error*
 TRITONSERVER_ServerOptionsSetGpuMetrics(
     TRITONSERVER_ServerOptions* options, bool gpu_metrics);
 
+/// Set the interval for metrics collection in a server options.
+///
+/// \param options The server options object.
+/// \param metrics_interval The time interval in seconds between
+/// successive metrics updates.
+/// \return a TRITONSERVER_Error indicating success or failure.
+TRITONSERVER_DECLSPEC TRITONSERVER_Error*
+TRITONSERVER_ServerOptionsSetMetricsInterval(
+    TRITONSERVER_ServerOptions* options, float metrics_interval);
+
 /// Set the directory containing backend shared libraries. This
 /// directory is searched last after the version and model directory
 /// in the model repository when looking for the backend shared
