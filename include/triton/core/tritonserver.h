626202b25333d9416a124e49ee859787c9a41f00
Kyle McGill <kmcgill@nvidia.com>
2022-04-11 17:41:11 -0700

    reverting version

--- include/triton/core/tritonserver.h
+++ include/triton/core/tritonserver.h
@@ -91,7 +91,7 @@ struct TRITONSERVER_MetricFamily;
 ///   }
 ///
 #define TRITONSERVER_API_VERSION_MAJOR 1
-#define TRITONSERVER_API_VERSION_MINOR 11
+#define TRITONSERVER_API_VERSION_MINOR 12
 
 /// Get the TRITONBACKEND API version supported by the Triton shared
 /// library. This value can be compared against the
