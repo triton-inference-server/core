5fd015f0e87910ae437a598d870e2712b9a37c47
Kyle McGill <kmcgill@nvidia.com>
2022-05-27 14:55:53 -0700

    clang-format

--- src/model_config_utils.cc
+++ src/model_config_utils.cc
@@ -967,17 +967,25 @@ AutoCompleteBackendFields(
   }
 
   // Custom Backend
-  // For now only do the narrowest case which is no info given in the model config
-  if (config->backend().empty() && config->platform().empty() && config->default_model_filename().empty()) {
-    LOG_VERBOSE(1) << "Could not infer triton supported backend so attempting autofill of custom backend.";
-    // Since we lazily load the backends, we let the model tell us what backend to load. 
-    // We must assume that if the model name conforms to the required shape, 
-    // we parse the backend name out of the model file name. i.e. model.identity
-    // will set the backend to "identity".
+  // For now only do the narrowest case which is no info given in the model
+  // config
+  if (config->backend().empty() && config->platform().empty() &&
+      config->default_model_filename().empty()) {
+    LOG_VERBOSE(1) << "Could not infer triton supported backend so attempting "
+                      "autofill of custom backend.";
+    // Since we lazily load the backends, we let the model tell us what backend
+    // to load. We must assume that if the model name conforms to the required
+    // shape, we parse the backend name out of the model file name. i.e.
+    // model.identity will set the backend to "identity".
     const std::string delimeter = ".";
     size_t pos = model_name.find(delimeter, 0);
     if (pos == std::string::npos) {
-      return Status(triton::common::Error::Code::INVALID_ARG, ("Invalid model name: Could not determine backend for model '" + model_name + "' with no backend in model configuration. Expected model name of the form 'model.<backend_name>'."));
+      return Status(
+          triton::common::Error::Code::INVALID_ARG,
+          ("Invalid model name: Could not determine backend for model '" +
+           model_name +
+           "' with no backend in model configuration. Expected model name of "
+           "the form 'model.<backend_name>'."));
     }
     const std::string backend_name = model_name.substr(pos, std::string::npos);
     config->set_backend(backend_name);
