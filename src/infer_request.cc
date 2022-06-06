74b203fbf5db3b120c61f85256689958f9a1318d
dyastremsky@nvidia.com <dyastremsky@nvidia.com>
2022-06-06 15:24:11 -0700

    Fixed scope.

--- src/infer_request.cc
+++ src/infer_request.cc
@@ -51,7 +51,7 @@ NullResponseAlloc(
 {
   return TRITONSERVER_ErrorNew(
       TRITONSERVER_ERROR_INTERNAL,
-      (IdString() + "unexpected allocation for null request, no output should be requested.").c_str());
+      "unexpected allocation for null request, no output should be requested.";
 }
 
 TRITONSERVER_Error*
@@ -62,7 +62,7 @@ NullResponseRelease(
 {
   return TRITONSERVER_ErrorNew(
       TRITONSERVER_ERROR_INTERNAL,
-      (IdString() + "unexpected release for null request, no output should be requested.").c_str());
+      "unexpected release for null request, no output should be requested.");
 }
 
 ResponseAllocator null_allocator = ResponseAllocator(
@@ -76,7 +76,7 @@ NullResponseComplete(
   if (iresponse != nullptr) {
     LOG_TRITONSERVER_ERROR(
         TRITONSERVER_InferenceResponseDelete(iresponse),
-        (IdString() + "deleting null response").c_str());
+        "deleting null response");
   }
 }
 
@@ -87,7 +87,7 @@ NullRequestComplete(
   if ((flags & TRITONSERVER_REQUEST_RELEASE_ALL) != 0) {
     LOG_TRITONSERVER_ERROR(
         TRITONSERVER_InferenceRequestDelete(request),
-        (IdString() + "deleting null request").c_str());
+        "deleting null request");
   }
 }
 
@@ -261,11 +261,11 @@ InferenceRequest::RespondIfError(
   std::unique_ptr<InferenceResponse> response;
   LOG_STATUS_ERROR(
       request->response_factory_.CreateResponse(&response),
-      (this->IdString() + "failed to create error response").c_str());
+      (request->IdString() + "failed to create error response").c_str());
   LOG_STATUS_ERROR(
       InferenceResponse::SendWithStatus(
           std::move(response), TRITONSERVER_RESPONSE_COMPLETE_FINAL, status),
-      (this->IdString() + "failed to send error response").c_str());
+      (request->IdString() + "failed to send error response").c_str());
 
   // If releasing the request then invoke the release callback which
   // gives ownership to the callback. So can't access 'request' after
@@ -362,7 +362,7 @@ InferenceRequest::CopyAsNull(const InferenceRequest& from)
 
     if (from_data_byte_size != byte_size) {
       LOG_WARNING
-          << this->IdString() << "The byte size of shape tensor to be copied does not match";
+          << lrequest->IdString() << "The byte size of shape tensor to be copied does not match";
     }
 
     // Copy the shape values to the input buffer
@@ -1248,7 +1248,7 @@ InferenceRequest::Input::SetData(const std::shared_ptr<Memory>& data)
   if (data_->TotalByteSize() != 0) {
     return Status(
         Status::Code::INVALID_ARG,
-        this->IdString() + "input '" + name_ + "' already has data, can't overwrite");
+        "input '" + name_ + "' already has data, can't overwrite");
   }
 
   data_ = data;
@@ -1263,7 +1263,7 @@ InferenceRequest::Input::SetData(
   if (host_policy_data_map_.find(host_policy_name) !=
       host_policy_data_map_.end()) {
     return Status(
-        Status::Code::INVALID_ARG, this->IdString() + "input '" + name_ +
+        Status::Code::INVALID_ARG, "input '" + name_ +
                                        "' already has data for host policy '" +
                                        host_policy_name + "', can't overwrite");
   }
