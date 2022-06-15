16757aafa37b04c813a1cbb64b80b4b6a277bb24
Ryan McCormick <rmccormick@nvidia.com>
2022-06-14 18:31:36 -0700

    Add thread_pool to unit test cmake libs, fix InitFourWorker test

--- src/test/async_work_queue_test.cc
+++ src/test/async_work_queue_test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+// Copyright 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -64,7 +64,7 @@ TEST_F(AsyncWorkQueueTest, InitOneWorker)
 
 TEST_F(AsyncWorkQueueTest, InitFourWorker)
 {
-  auto error = tc::AsyncWorkQueue::Initialize(1);
+  auto error = tc::AsyncWorkQueue::Initialize(4);
   EXPECT_TRUE(error.IsOk()) << error.Message();
 }
 
