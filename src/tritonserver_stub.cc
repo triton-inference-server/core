0023967eed69c0bdf743a9d287e92970d044856e
fpetrini15 <francescogpetrini@gmail.com>
2022-07-14 18:08:28 -0700

    Added to stub file

--- src/tritonserver_stub.cc
+++ src/tritonserver_stub.cc
@@ -386,6 +386,10 @@ TRITONSERVER_ServerOptionsSetModelLoadThreadCount()
 {
 }
 TRITONAPI_DECLSPEC void
+TRITONSERVER_ServerOptionsSetLogOutFile()
+{
+}
+TRITONAPI_DECLSPEC void
 TRITONSERVER_ServerOptionsSetLogInfo()
 {
 }
