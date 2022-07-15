e527b77eaf2633eadd5b35ae0125cd2250b2c21e
Kris Hung <krish@nvidia.com>
2022-07-15 11:14:09 -0700

    Fix typo (#97)

--- src/infer_parameter.h
+++ src/infer_parameter.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+// Copyright 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -60,7 +60,7 @@ class InferenceParameter {
   {
   }
 
-  // The name of the parametre.
+  // The name of the parameter.
   const std::string& Name() const { return name_; }
 
   // Data type of the parameter.
