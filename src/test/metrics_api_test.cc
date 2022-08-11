4be9dd0c199b9b55f63846fbe6955df4f6338e47
Ryan McCormick <rmccormick@nvidia.com>
2022-08-11 15:14:18 -0700

    Add more metrics API unit tests for duplicate metric family / metric entries and update label hasher for latest prometheus version

--- src/test/metrics_api_test.cc
+++ src/test/metrics_api_test.cc
@@ -43,8 +43,12 @@ namespace {
         << TRITONSERVER_ErrorMessage(err__.get());                            \
   } while (false)
 
-// Helpers
-void PrintMetrics(TRITONSERVER_Server* server, std::string substr) {
+/* Helpers */
+
+// Get serialized metrics string from C API
+void
+GetMetrics(TRITONSERVER_Server* server, std::string* metrics_str)
+{
   // Check metrics via C API
   ASSERT_NE(server, nullptr);
   TRITONSERVER_Metrics* metrics = nullptr;
@@ -56,17 +60,82 @@ void PrintMetrics(TRITONSERVER_Server* server, std::string substr) {
       TRITONSERVER_MetricsFormatted(
           metrics, TRITONSERVER_METRIC_PROMETHEUS, &base, &byte_size),
       "format metrics string");
-  auto metrics_str = std::string(base, byte_size);
+  *metrics_str = std::string(base, byte_size);
+}
 
-  // Only print substr if provided and found in output
-  if (!substr.empty()) {
-      auto found = metrics_str.find(std::string(substr));
-      ASSERT_NE(found, std::string::npos);
-      std::cout << "======" << std::endl;
-      std::cout << metrics_str.substr(found, std::string::npos) << std::endl;
-  } else {
-      std::cout << metrics_str << std::endl;
+// Count number of times substr appears in s
+int
+CountMatches(const std::string s, const std::string substr)
+{
+  int num_matches = 0;
+  std::string::size_type pos = 0;
+  while ((pos = s.find(substr, pos)) != std::string::npos) {
+    num_matches++;
+    pos += substr.length();
   }
+  return num_matches;
+}
+
+// Add two metrics with the same labels from the same metric family
+// and verify they refer to the same metric/value
+void
+DupeMetricHelper(std::vector<const TRITONSERVER_Parameter*> labels)
+{
+  // Create metric family
+  TRITONSERVER_MetricFamily* family;
+  TRITONSERVER_MetricKind kind = TRITONSERVER_METRIC_KIND_COUNTER;
+  const char* name = "dupe_metric_test";
+  const char* description = "dupe metric description";
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricFamilyNew(&family, kind, name, description),
+      "Creating new metric family1");
+
+  // Create metric
+  TRITONSERVER_Metric* metric1;
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricNew(&metric1, family, labels.data(), labels.size()),
+      "Creating new metric");
+
+  // Create duplicate metric
+  TRITONSERVER_Metric* metric2;
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricNew(&metric2, family, labels.data(), labels.size()),
+      "Creating new metric");
+
+  // Verify dupe metrics are different pointers
+  ASSERT_NE(metric1, metric2);
+
+  // Verify dupe metrics reference same underlying metric
+  double value1 = -1;
+  double value2 = -1;
+  double inc = 7.5;
+
+  // Verify initial values of zero
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricValue(metric1, &value1),
+      "query metric value after increment");
+  ASSERT_EQ(value1, 0);
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricValue(metric2, &value2),
+      "query metric value after increment");
+  ASSERT_EQ(value2, 0);
+
+  // Increment metric 1, check metric 2 == metric 1
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricIncrement(metric1, inc), "increase metric value");
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricValue(metric1, &value1),
+      "query metric value after increment");
+  // Verify increment worked
+  ASSERT_EQ(value1, inc);
+
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricValue(metric2, &value2),
+      "query metric value after increment");
+  // Verify metric values are equal
+  ASSERT_EQ(value1, value2);
+  std::cout << "metric1 value: " << value1 << " == metric2 value: " << value2
+            << std::endl;
 }
 
 // Test Fixture
@@ -107,8 +176,6 @@ class MetricsApiTest : public ::testing::Test {
         "deleting server options");
   }
 
-  // TODO: See if prometheus registry is actually getting cleared or not across
-  // server deletions.
   // Run after each test
   void TearDown() override
   {
@@ -131,7 +198,7 @@ TEST_F(MetricsApiTest, TestCounterEndToEnd)
   // Create metric family
   TRITONSERVER_MetricFamily* family;
   TRITONSERVER_MetricKind kind = TRITONSERVER_METRIC_KIND_COUNTER;
-  const char* name = "api_counter_example";
+  const char* name = "custom_counter_example";
   const char* description = "this is an example counter metric added via API.";
   FAIL_TEST_IF_ERR(
       TRITONSERVER_MetricFamilyNew(&family, kind, name, description),
@@ -182,25 +249,12 @@ TEST_F(MetricsApiTest, TestCounterEndToEnd)
       TRITONSERVER_GetMetricKind(metric, &kind_tmp), "query metric kind");
   ASSERT_EQ(kind_tmp, kind);
 
-  // Check metrics via C API
-  ASSERT_NE(server_, nullptr);
-  TRITONSERVER_Metrics* metrics = nullptr;
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_ServerMetrics(server_, &metrics), "fetch metrics");
-  const char* base;
-  size_t byte_size;
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricsFormatted(
-          metrics, TRITONSERVER_METRIC_PROMETHEUS, &base, &byte_size),
-      "format metrics string");
-  auto metrics_str = std::string(base, byte_size);
-
   // Assert custom metric is reported and found in output
-  auto found = metrics_str.find(std::string("# HELP ") + name);
+  std::string metrics_str;
+  GetMetrics(server_, &metrics_str);
   std::cout << metrics_str << std::endl;
-  ASSERT_NE(found, std::string::npos);
-  std::cout << "======" << std::endl;
-  std::cout << metrics_str.substr(found, std::string::npos) << std::endl;
+  const int num_matches = CountMatches(metrics_str, description);
+  ASSERT_EQ(num_matches, 1);
 
   // Cleanup
   FAIL_TEST_IF_ERR(TRITONSERVER_MetricDelete(metric), "delete metric");
@@ -214,7 +268,7 @@ TEST_F(MetricsApiTest, TestGaugeEndToEnd)
   // Create metric family
   TRITONSERVER_MetricFamily* family;
   TRITONSERVER_MetricKind kind = TRITONSERVER_METRIC_KIND_GAUGE;
-  const char* name = "api_gauge_example";
+  const char* name = "custom_gauge_example";
   const char* description = "this is an example gauge metric added via API.";
   FAIL_TEST_IF_ERR(
       TRITONSERVER_MetricFamilyNew(&family, kind, name, description),
@@ -273,25 +327,11 @@ TEST_F(MetricsApiTest, TestGaugeEndToEnd)
       TRITONSERVER_GetMetricKind(metric, &kind_tmp), "query metric kind");
   ASSERT_EQ(kind_tmp, kind);
 
-  // Check metrics via C API
-  ASSERT_NE(server_, nullptr);
-  TRITONSERVER_Metrics* metrics = nullptr;
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_ServerMetrics(server_, &metrics), "fetch metrics");
-  const char* base;
-  size_t byte_size;
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricsFormatted(
-          metrics, TRITONSERVER_METRIC_PROMETHEUS, &base, &byte_size),
-      "format metrics string");
-  auto metrics_str = std::string(base, byte_size);
-
   // Assert custom metric is reported and found in output
-  auto found = metrics_str.find(std::string("# HELP ") + name);
-  std::cout << metrics_str << std::endl;
-  ASSERT_NE(found, std::string::npos);
-  std::cout << "======" << std::endl;
-  std::cout << metrics_str.substr(found, std::string::npos) << std::endl;
+  std::string metrics_str;
+  GetMetrics(server_, &metrics_str);
+  const int num_matches = CountMatches(metrics_str, description);
+  ASSERT_EQ(num_matches, 1);
 
   // Cleanup
   FAIL_TEST_IF_ERR(TRITONSERVER_MetricDelete(metric), "delete metric");
@@ -299,67 +339,90 @@ TEST_F(MetricsApiTest, TestGaugeEndToEnd)
       TRITONSERVER_MetricFamilyDelete(family), "delete metric family");
 }
 
-// Test duplicate metric family names with different descriptions
+// Test that a duplicate metric family can't be added
+// with a conflicting type/kind
+TEST_F(MetricsApiTest, TestDupeMetricFamilyDiffKind)
+{
+  // Create metric family
+  TRITONSERVER_MetricFamily* family1;
+  TRITONSERVER_MetricKind kind1 = TRITONSERVER_METRIC_KIND_COUNTER;
+  const char* name = "diff_kind_test";
+  const char* description = "diff kind description";
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricFamilyNew(&family1, kind1, name, description),
+      "Creating new metric family1");
+
+  // Create duplicate metric family with different kind
+  TRITONSERVER_MetricFamily* family2;
+  TRITONSERVER_MetricKind kind2 = TRITONSERVER_METRIC_KIND_GAUGE;
+  // Expect this to fail, can't have duplicate name of different kind
+  auto err = TRITONSERVER_MetricFamilyNew(&family2, kind2, name, description);
+  ASSERT_NE(err, nullptr);
+}
+
+// Test that a duplicate metric family name will still
+// return the original metric family even if the description
+// is changed
 TEST_F(MetricsApiTest, TestDupeMetricFamilyDiffDescription)
 {
   // Create metric family
   TRITONSERVER_MetricFamily* family1;
   TRITONSERVER_MetricKind kind = TRITONSERVER_METRIC_KIND_COUNTER;
-  const char* name = "api_counter_example";
-  const char* description1 = "this is an example counter metric added via API.";
+  const char* name = "diff_description_test";
+  const char* description1 = "first description";
   FAIL_TEST_IF_ERR(
       TRITONSERVER_MetricFamilyNew(&family1, kind, name, description1),
       "Creating new metric family1");
 
   // Create duplicate metric family
   TRITONSERVER_MetricFamily* family2;
-  const char* description2 = "this is a different description.";
+  const char* description2 = "second description";
   FAIL_TEST_IF_ERR(
       TRITONSERVER_MetricFamilyNew(&family2, kind, name, description2),
       "Creating new metric family2");
 
-  std::cout << "family1 address: 0x" << family1 << std::endl;
-  std::cout << "family2 address: 0x" << family2 << std::endl;
-  // TODO
-  ASSERT_NE(family1, family2);
-  PrintMetrics(server_, "");
+  // Assert MetricFamily is not reported until metrics are added to them
+  std::string metrics_str;
+  GetMetrics(server_, &metrics_str);
+  int num_matches1 = CountMatches(metrics_str, description1);
+  int num_matches2 = CountMatches(metrics_str, description2);
+  ASSERT_EQ(num_matches1, 0);
+  ASSERT_EQ(num_matches2, 0);
+
+  // Add metric to family1
+  std::vector<const TRITONSERVER_Parameter*> labels;
+  TRITONSERVER_Metric* metric1;
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricNew(&metric1, family1, labels.data(), labels.size()),
+      "Creating new metric1");
+
+  // Add metric for family2
+  TRITONSERVER_Metric* metric2;
+  FAIL_TEST_IF_ERR(
+      TRITONSERVER_MetricNew(&metric2, family2, labels.data(), labels.size()),
+      "Creating new metric2");
+
+  // Assert MetricFamily is reported exactly once
+  // This confirms attempting to add a duplicate returns the existing family
+  GetMetrics(server_, &metrics_str);
+  std::cout << metrics_str << std::endl;
+  num_matches1 = CountMatches(metrics_str, description1);
+  num_matches2 = CountMatches(metrics_str, description2);
+  // The first description will be taken/kept if adding a duplicate
+  /// metric family name, even with a different description
+  ASSERT_EQ(num_matches1, 1);
+  ASSERT_EQ(num_matches2, 0);
 }
 
-// Test duplicate metric family names with different descriptions
-TEST_F(MetricsApiTest, TestDupeMetricFamilyDiffKind)
-{
-  // Create metric family
-  TRITONSERVER_MetricFamily* family1;
-  TRITONSERVER_MetricKind kind1 = TRITONSERVER_METRIC_KIND_COUNTER;
-  const char* name = "api_example";
-  const char* description = "this is an example counter metric added via API.";
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricFamilyNew(&family1, kind1, name, description),
-      "Creating new metric family1");
-
-  // Create duplicate metric family
-  TRITONSERVER_MetricFamily* family2;
-  TRITONSERVER_MetricKind kind2 = TRITONSERVER_METRIC_KIND_GAUGE;
-  // TODO: THIS SHOULD FAIL
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricFamilyNew(&family2, kind2, name, description),
-      "Creating new metric family2");
-
-  std::cout << "family1 address: 0x" << family1 << std::endl;
-  std::cout << "family2 address: 0x" << family2 << std::endl;
-  // TODO
-  ASSERT_NE(family1, family2);
-  PrintMetrics(server_, "");
-}
-
-// Test duplicate metric family objects
+// Test that adding a duplicate metric family will reuse the original
+// and not add another entry to registry
 TEST_F(MetricsApiTest, TestDupeMetricFamily)
 {
   // Create metric family
   TRITONSERVER_MetricFamily* family1;
   TRITONSERVER_MetricKind kind = TRITONSERVER_METRIC_KIND_COUNTER;
-  const char* name = "api_counter_example";
-  const char* description = "this is an example counter metric added via API.";
+  const char* name = "dupe_metric_family_test";
+  const char* description = "dupe metric family description";
   FAIL_TEST_IF_ERR(
       TRITONSERVER_MetricFamilyNew(&family1, kind, name, description),
       "Creating new metric family1");
@@ -370,90 +433,58 @@ TEST_F(MetricsApiTest, TestDupeMetricFamily)
       TRITONSERVER_MetricFamilyNew(&family2, kind, name, description),
       "Creating new metric family2");
 
-  std::cout << "family1 address: 0x" << family1 << std::endl;
-  std::cout << "family2 address: 0x" << family2 << std::endl;
-  // TODO
-  ASSERT_NE(family1, family2);
-  PrintMetrics(server_, "");
-}
+  // Assert MetricFamily is not reported until metrics are added to them
+  std::string metrics_str;
+  GetMetrics(server_, &metrics_str);
+  std::cout << metrics_str << std::endl;
+  int num_matches = CountMatches(metrics_str, description);
+  ASSERT_EQ(num_matches, 0);
 
-// Test duplicate metric objects
-TEST_F(MetricsApiTest, TestDupeMetricLabels)
-{
-  // Create metric family
-  TRITONSERVER_MetricFamily* family;
-  TRITONSERVER_MetricKind kind = TRITONSERVER_METRIC_KIND_COUNTER;
-  const char* name = "api_counter_example";
-  const char* description = "this is an example counter metric added via API.";
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricFamilyNew(&family, kind, name, description),
-      "Creating new metric family1");
-
-  // Create metric
-  TRITONSERVER_Metric* metric1;
+  // Add metric to family1
   std::vector<const TRITONSERVER_Parameter*> labels;
-  labels.emplace_back(TRITONSERVER_ParameterNew(
-      "example1", TRITONSERVER_PARAMETER_STRING, "counter_label1"));
-  labels.emplace_back(TRITONSERVER_ParameterNew(
-      "example2", TRITONSERVER_PARAMETER_STRING, "counter_label2"));
+  TRITONSERVER_Metric* metric1;
   FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricNew(&metric1, family, labels.data(), labels.size()),
-      "Creating new metric");
+      TRITONSERVER_MetricNew(&metric1, family1, labels.data(), labels.size()),
+      "Creating new metric1");
 
-  // Create duplicate metric
+  // Add metric for family2
   TRITONSERVER_Metric* metric2;
   FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricNew(&metric2, family, labels.data(), labels.size()),
-      "Creating new metric");
+      TRITONSERVER_MetricNew(&metric2, family2, labels.data(), labels.size()),
+      "Creating new metric2");
+
+  // Assert MetricFamily is reported exactly once
+  // This confirms attempting to add a duplicate returns the existing family
+  GetMetrics(server_, &metrics_str);
+  std::cout << metrics_str << std::endl;
+  num_matches = CountMatches(metrics_str, description);
+  ASSERT_EQ(num_matches, 1);
+}
+
+// Test that adding a duplicate metric will refer to the same
+// underlying metric, and all instances will be updated
+TEST_F(MetricsApiTest, TestDupeMetricLabels)
+{
+  std::vector<const TRITONSERVER_Parameter*> labels;
+  labels.emplace_back(TRITONSERVER_ParameterNew(
+      "example1", TRITONSERVER_PARAMETER_STRING, "label1"));
+  labels.emplace_back(TRITONSERVER_ParameterNew(
+      "example2", TRITONSERVER_PARAMETER_STRING, "label2"));
+
+  DupeMetricHelper(labels);
 
   // Cleanup
   for (const auto label : labels) {
     TRITONSERVER_ParameterDelete(const_cast<TRITONSERVER_Parameter*>(label));
   }
-
-  std::cout << "metric1 address: 0x" << metric1 << std::endl;
-  std::cout << "metric2 address: 0x" << metric2 << std::endl;
-  // Verify dupe metrics are different pointers
-  ASSERT_NE(metric1, metric2);
-
-  // Verify dupe metrics reference same underlying metric
-  double value1 = -1;
-  double value2 = -1;
-  double inc = 7.5;
-
-  // Verify initial values of zero
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricValue(metric1, &value1),
-      "query metric value after increment");
-  ASSERT_EQ(value1, 0);
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricValue(metric2, &value2),
-      "query metric value after increment");
-  ASSERT_EQ(value2, 0);
-
-  // Increment metric 1, check metric 2 == metric 1
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricIncrement(metric1, inc), "increase metric value");
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricValue(metric1, &value1),
-      "query metric value after increment");
-  // Verify increment worked
-  ASSERT_EQ(value1, inc);
-
-  FAIL_TEST_IF_ERR(
-      TRITONSERVER_MetricValue(metric2, &value2),
-      "query metric value after increment");
-  // Verify metric values are equal
-  ASSERT_EQ(value1, value2);
-  std::cout << "metric1 value: " << value1 << " == metric2 value: " << value2
-            << std::endl;
 }
 
-// Test duplicate metric objects without labels
-TEST_F(MetricsApiTest, TestDupeMetricNoLabels)
+// Test that adding a duplicate metric will refer to the same
+// underlying metric, and all instances will be updated
+TEST_F(MetricsApiTest, TestDupeMetricEmptyLabels)
 {
-  // TODO: same as above with empty labels
-  ASSERT_EQ(true, false);
+  std::vector<const TRITONSERVER_Parameter*> labels;
+  DupeMetricHelper(labels);
 }
 
 }  // namespace
