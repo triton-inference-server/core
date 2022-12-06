7a754d2749df45049fd2097aee4aeacb097e04a6
Ryan McCormick <rmccormick@nvidia.com>
2022-12-05 18:47:01 -0800

    Pass cache config as TRITONSERVER_Message to share library

--- src/cache_manager.h
+++ src/cache_manager.h
@@ -95,7 +95,8 @@ class TritonCache {
 
   // dlopen / dlsym handles
   void* dlhandle_;
-  typedef TRITONSERVER_Error* (*TritonCacheInitFn_t)(TRITONCACHE_Cache** cache);
+  typedef TRITONSERVER_Error* (*TritonCacheInitFn_t)(
+      TRITONCACHE_Cache** cache, TRITONSERVER_Message* config);
   TritonCacheInitFn_t init_fn_;
   typedef TRITONSERVER_Error* (*TritonCacheFiniFn_t)(TRITONCACHE_Cache* cache);
   TritonCacheFiniFn_t fini_fn_;
