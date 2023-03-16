99a6c3eaf97ebfbd71f72a941a0505546b5aa75b
Xiangyu Guo <xiaguo@microsoft.com>
2023-03-16 20:26:55 +0000

    Default value

--- include/triton/core/tritonbackend.h
+++ include/triton/core/tritonbackend.h
@@ -206,7 +206,7 @@ TRITONBACKEND_DECLSPEC TRITONSERVER_Error* TRITONBACKEND_InputProperties(
     TRITONBACKEND_Input* input, const char** name,
     TRITONSERVER_DataType* datatype, const int64_t** shape,
     uint32_t* dims_count, uint64_t* byte_size, uint32_t* buffer_count,
-    bool* is_initializer_tensor);
+    bool* is_initializer_tensor = nullptr);
 
 /// Get the name and properties of an input tensor associated with a given
 /// host policy. If there are no input buffers for the specified  host policy,
