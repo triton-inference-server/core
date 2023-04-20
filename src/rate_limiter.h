ecbe0230d5770481d23c5f1f984ac6fac651e1df
Tanmay Verma <tanmay2592@gmail.com>
2023-04-20 11:56:22 -0700

    Serialize the model instance registration in resource manager (#194)
    * Serialize the model instance registration in resource manager
    
    * Fix the comment
    
    * Update copyright

--- src/rate_limiter.h
+++ src/rate_limiter.h
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright 2020-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -314,6 +314,7 @@ class RateLimiter {
 
   // Manager to keep track of the resource allocations
   std::unique_ptr<ResourceManager> resource_manager_;
+  std::mutex resource_manager_mtx_;
 
   // Mutex to serialize Payload [de]allocation
   std::mutex payload_mu_;
