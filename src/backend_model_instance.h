0f44ef500a7182e2e50f8bcebe87a7af8d270064
GuanLuo <gluo@nvidia.com>
2023-05-10 10:52:48 -0700

    Fix include

--- src/backend_model_instance.h
+++ src/backend_model_instance.h
@@ -25,6 +25,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
+#include <boost/core/span.hpp>
 #include <functional>
 #include <future>
 #include <memory>
