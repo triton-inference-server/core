13c89857def0edf0b156af48bad89e608199f4cf
Tanmay Verma <tanmay2592@gmail.com>
2023-07-06 12:02:49 -0700

    Allow reserved values for platform field when using python backend (#223)
    * Relax the checks on platform field for unknown backends
    
    * Fix comment

--- src/model_config_utils.cc
+++ src/model_config_utils.cc
@@ -1311,9 +1311,10 @@ ValidateModelConfig(
   }
 
   // Ensure both platform and backend are referring to known backend,
-  // or both referring to unknown backend for user-provided backend.
-  if (GetBackendTypeFromPlatform(config.platform()) !=
-      GetBackendType(config.backend())) {
+  // and allow all platforms for a user-provided unknown backend.
+  auto backend_type = GetBackendType(config.backend());
+  if ((backend_type != BackendType::BACKEND_TYPE_UNKNOWN) &&
+      (backend_type != GetBackendTypeFromPlatform(config.platform()))) {
     return Status(
         Status::Code::INVALID_ARG,
         "unexpected 'platform' and 'backend' pair, got:" + config.platform() +
