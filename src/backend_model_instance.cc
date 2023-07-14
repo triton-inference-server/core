449107ef4387c1da48d8c1a2c7b1a664b69d8362
Jacky <18255193+kthui@users.noreply.github.com>
2023-07-14 16:22:29 -0700

    Unify default naming on different instance group count (#231)
    * Add name into instance signature
    
    * Use actual instance name
    
    * Unify instance name across different group count
    
    * Use different instance name unification method
    
    * Revert "Add name into instance signature" and "Use actual instance name"

--- src/backend_model_instance.cc
+++ src/backend_model_instance.cc
@@ -219,9 +219,7 @@ TritonModelInstance::SetInstances(
           secondary_device.device_id());
     }
     for (int32_t c = 0; c < group.count(); ++c) {
-      std::string instance_name{
-          group.count() > 1 ? group.name() + "_" + std::to_string(c)
-                            : group.name()};
+      std::string instance_name{group.name() + "_" + std::to_string(c)};
       const bool passive = group.passive();
       std::vector<std::tuple<
           std::string, TRITONSERVER_InstanceGroupKind, int32_t,
