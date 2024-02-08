c920b3bc7fe80aa55011622deb24abce277c996b
kthui <18255193+kthui@users.noreply.github.com>
2024-02-07 18:31:02 -0800

    Backend api version 1.19

--- include/triton/core/tritonbackend.h
+++ include/triton/core/tritonbackend.h
@@ -94,7 +94,7 @@ struct TRITONBACKEND_Batcher;
 ///   }
 ///
 #define TRITONBACKEND_API_VERSION_MAJOR 1
-#define TRITONBACKEND_API_VERSION_MINOR 18
+#define TRITONBACKEND_API_VERSION_MINOR 19
 
 /// Get the TRITONBACKEND API version supported by Triton. This value
 /// can be compared against the TRITONBACKEND_API_VERSION_MAJOR and
