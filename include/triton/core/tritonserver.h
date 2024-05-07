4f1ee4d974280e107921686b4f744dac3ffc532f
Yingge He <yinggeh@nvidia.com>
2024-05-06 22:18:26 -0700

    Increment API version

--- include/triton/core/tritonserver.h
+++ include/triton/core/tritonserver.h
@@ -91,7 +91,7 @@ struct TRITONSERVER_MetricFamily;
 ///   }
 ///
 #define TRITONSERVER_API_VERSION_MAJOR 1
-#define TRITONSERVER_API_VERSION_MINOR 30
+#define TRITONSERVER_API_VERSION_MINOR 31
 
 /// Get the TRITONBACKEND API version supported by the Triton shared
 /// library. This value can be compared against the
