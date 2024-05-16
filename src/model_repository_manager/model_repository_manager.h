4d093f2409ede3771814898ab6ef3d78361ece91
Tanmay Verma <tanmay2592@gmail.com>
2024-05-16 15:56:11 -0700

    Multiple Model Configurations (#348) (#357)
    Add `--mode-config-name` option when starting Triton server. Allow users to create multiple configurations and select a custom configuration other than the default `model/config.pbtxt`.
    
    Co-authored-by: Yingge He <157551214+yinggeh@users.noreply.github.com>

--- src/model_repository_manager/model_repository_manager.h
+++ src/model_repository_manager/model_repository_manager.h
@@ -1,4 +1,4 @@
-// Copyright 2018-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright 2018-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -79,10 +79,10 @@ class ModelRepositoryManager {
     ModelInfo(
         const std::pair<int64_t, int64_t>& mtime_nsec,
         const std::pair<int64_t, int64_t>& prev_mtime_ns,
-        const std::string& model_path)
+        const std::string& model_path, const std::string& model_config_path)
         : mtime_nsec_(mtime_nsec), prev_mtime_ns_(prev_mtime_ns),
           explicitly_load_(true), model_path_(model_path),
-          is_config_provided_(false)
+          model_config_path_(model_config_path), is_config_provided_(false)
     {
     }
     ModelInfo()
@@ -97,6 +97,7 @@ class ModelRepositoryManager {
     bool explicitly_load_;
     inference::ModelConfig model_config_;
     std::string model_path_;
+    std::string model_config_path_;
     // Temporary location to hold agent model list before creating the model
     // the ownership must transfer to ModelLifeCycle to ensure
     // the agent model life cycle is handled properly.
@@ -391,6 +392,8 @@ class ModelRepositoryManager {
   /// and the models in the model repository will not be loaded at startup.
   /// Otherwise, LoadUnloadModel() is not allowed and the models will be loaded.
   /// Cannot be set to true if polling_enabled is true.
+  /// \param model_config_name Custom model config name to load for all models.
+  /// Fall back to default config file if empty.
   /// \param life_cycle_options The options to configure ModelLifeCycle.
   /// \param model_repository_manager Return the model repository manager.
   /// \return The error status.
@@ -398,8 +401,8 @@ class ModelRepositoryManager {
       InferenceServer* server, const std::string& server_version,
       const std::set<std::string>& repository_paths,
       const std::set<std::string>& startup_models,
-      const bool strict_model_config, const bool polling_enabled,
-      const bool model_control_enabled,
+      const bool strict_model_config, const std::string& model_config_name,
+      const bool polling_enabled, const bool model_control_enabled,
       const ModelLifeCycleOptions& life_cycle_options,
       const bool enable_model_namespacing,
       std::unique_ptr<ModelRepositoryManager>* model_repository_manager);
@@ -410,7 +413,8 @@ class ModelRepositoryManager {
   Status PollAndUpdate();
 
   /// Load or unload a specified model.
-  /// \param models The models and the parameters to be loaded or unloaded
+  /// \param models The models and the parameters to be loaded or unloaded.
+  /// Expect the number of models to be exactly one.
   /// \param type The type action to be performed. If the action is LOAD and
   /// the model has been loaded, the model will be re-loaded.
   /// \return error status. Return "NOT_FOUND" if it tries to load
@@ -509,8 +513,9 @@ class ModelRepositoryManager {
 
   ModelRepositoryManager(
       const std::set<std::string>& repository_paths, const bool autofill,
-      const bool polling_enabled, const bool model_control_enabled,
-      const double min_compute_capability, const bool enable_model_namespacing,
+      const std::string& model_config_name, const bool polling_enabled,
+      const bool model_control_enabled, const double min_compute_capability,
+      const bool enable_model_namespacing,
       std::unique_ptr<ModelLifeCycle> life_cycle);
 
   /// The internal function that are called in Create() and PollAndUpdate().
@@ -614,6 +619,7 @@ class ModelRepositoryManager {
       const std::vector<const InferenceParameter*>& model_params);
 
   const bool autofill_;
+  const std::string model_config_name_;
   const bool polling_enabled_;
   const bool model_control_enabled_;
   const double min_compute_capability_;
