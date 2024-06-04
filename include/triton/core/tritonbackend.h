e655dc381abb9575d279c55d389e926836eda6a2
oandreeva-nv <oandreeva@nvidia.com>
2024-06-04 14:39:42 -0700

    fixed pre-commit

--- include/triton/core/tritonbackend.h
+++ include/triton/core/tritonbackend.h
@@ -1722,9 +1722,9 @@ TRITONBACKEND_BackendAttributeSetParallelModelInstanceLoading(
 ///
 /// \param batcher User-defined placeholder for backend to store and
 /// retrieve information about the batching strategy for this
-/// model. TRITONBACKEND_ISPEC returns a TRITONSERVER_Error indicating success or
-/// failure. \param model The backend model for which Triton is forming a batch.
-/// \return a TRITONSERVER_Error indicating success or failure.
+/// model. TRITONBACKEND_ISPEC returns a TRITONSERVER_Error indicating success
+/// or failure. \param model The backend model for which Triton is forming a
+/// batch. \return a TRITONSERVER_Error indicating success or failure.
 TRITONBACKEND_ISPEC TRITONSERVER_Error* TRITONBACKEND_ModelBatcherInitialize(
     TRITONBACKEND_Batcher** batcher, TRITONBACKEND_Model* model);
 
