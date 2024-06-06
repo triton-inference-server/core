272ef2905d9a2bcaa57215794ab98aa96a35cec9
Yingge He <yinggeh@nvidia.com>
2024-06-06 12:04:29 -0700

    Update header

--- src/infer_request.h
+++ src/infer_request.h
@@ -749,7 +749,8 @@ class InferenceRequest {
 
   Status ValidateBytesInputs(
       const std::string& input_id, const Input& input,
-      int64_t* const expected_byte_size) const;
+      int64_t* const expected_byte_size,
+      TRITONSERVER_MemoryType* buffer_memory_type) const;
 
   // Helpers for pending request metrics
   void IncrementPendingRequestCount();
