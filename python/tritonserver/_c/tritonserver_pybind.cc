a950a30764aa9054fa8efd31202eaf180c825d54
Krishnan Prashanth <kprashanth@nvidia.com>
2024-08-29 15:16:03 -0700

    Using lambda

--- python/tritonserver/_c/tritonserver_pybind.cc
+++ python/tritonserver/_c/tritonserver_pybind.cc
@@ -2112,7 +2112,9 @@ PYBIND11_MODULE(triton_bindings, m)
       .export_values();
   py::class_<PyServer>(m, "TRITONSERVER_Server")
       .def(py::init<PyServerOptions&>())
-      .def("_ptr", &PyServer::_ptr)
+      .def(
+          "_ptr",
+          &(PyServer & server) { reinterpret_cast<uintptr_t>(server.Ptr();) })
       .def("stop", &PyServer::Stop)
       .def("register_model_repository", &PyServer::RegisterModelRepository)
       .def("unregister_model_repository", &PyServer::UnregisterModelRepository)
