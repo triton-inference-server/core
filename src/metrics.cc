1962cceda8254ecb1de39736f0f926da3992ae50
Indrajit Bhosale <iamindrajitb@gmail.com>
2024-10-21 10:03:48 -0700

    feat: Add model_load_time metric (#397)
    Model Load time Metric added

--- src/metrics.cc
+++ src/metrics.cc
@@ -109,6 +109,11 @@ Metrics::Metrics()
                     "execution per-model.")
               .Register(*registry_)),
 
+      model_load_time_family_(prometheus::BuildGauge()
+                                  .Name("nv_model_load_duration_secs")
+                                  .Help("Model load time in seconds")
+                                  .Register(*registry_)),
+
       pinned_memory_pool_total_family_(
           prometheus::BuildGauge()
               .Name("nv_pinned_memory_pool_total_bytes")
