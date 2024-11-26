ce46b95dcaa860a524f91324c146877c018dcf13
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2024-11-26 15:33:05 -0800

    Enable support for Ubuntu 24.04 (#409) (#415)
    * Build Updates for Ubuntu24.04 (#408)
    
    * update json dir path
    
    * remove werror arg
    
    * Revert "remove werror arg"
    
    This reverts commit 3fd957ab3c7386ec7a430e2caa4245d93d67e73c.
    
    * skip warning error
    
    * Update python version
    
    ---------
    
    Co-authored-by: Anant Sharma <anants@nvidia.com>

--- python/setup.py
+++ python/setup.py
@@ -97,6 +97,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
         "Environment :: Console",
         "Natural Language :: English",
         "Operating System :: OS Independent",
